5c4bc0d35a9d3c821d096191d7f29fd38a55dc8a
Jon Delano <jd31068@hotmail.com>
2022-07-24 13:54:45 -0400

    adding bulletlist widget - cleaned up a few msgs

--- lib/pages/podcast_guest.dart
+++ lib/pages/podcast_guest.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 import '/widgets/latestcard.dart';
-import 'dart:math' as math;
 
 class PodcastGuest extends StatefulWidget {
   const PodcastGuest({Key? key}) : super(key: key);
@@ -58,35 +57,4 @@ List<Container> theLatestItems(theLatestItems) {
       child: thelatestcard(theLatestItems[index]),
     ),
   );
-}
-
-class _SliverAppBarDelegate extends SliverPersistentHeaderDelegate {
-  _SliverAppBarDelegate({
-    required this.minHeight,
-    required this.maxHeight,
-    required this.child,
-  });
-
-  final double minHeight;
-  final double maxHeight;
-  final Widget child;
-
-  @override
-  double get minExtent => minHeight;
-
-  @override
-  double get maxExtent => math.max(maxHeight, minHeight);
-
-  @override
-  Widget build(
-      BuildContext context, double shrinkOffset, bool overlapsContent) {
-    return SizedBox.expand(child: child);
-  }
-
-  @override
-  bool shouldRebuild(_SliverAppBarDelegate oldDelegate) {
-    return maxHeight != oldDelegate.maxHeight ||
-        minHeight != oldDelegate.minHeight ||
-        child != oldDelegate.child;
-  }
 }
